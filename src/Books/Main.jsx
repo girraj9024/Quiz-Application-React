b54e97128e12c5d135d9931a4ced64aa7973e515
G.K.Kumar☜😎👈 <g.k.kumar9024@gmail.com>
2024-05-03 12:07:03 +0530

    first commit

--- src/Books/Main.jsx
+++ src/Books/Main.jsx
@@ -3,6 +3,8 @@ import './style.css'
 
 function Main() {
     const [booksBySubjects, setBooksBySubjects] = useState([]);
+    const [searchTerm, setSearchTerm] = useState("");
+    const [searchResults, setSearchResults] = useState([]);
 
     useEffect(() => {
         const fetchBooks = async () => {
@@ -12,7 +14,7 @@ function Main() {
                     "https://openlibrary.org/subjects/love.json?details=true",
                     "https://openlibrary.org/subjects/kids.json?details=true",
                     "https://openlibrary.org/subjects/romance.json?details=true",
-];
+                ];
 
                 const responses = await Promise.all(urls.map(url => fetch(url)));
                 const results = await Promise.all(responses.map(response => response.json()));
@@ -26,26 +28,41 @@ function Main() {
         fetchBooks();
     }, []);
 
+    useEffect(() => {
+        const filteredResults = booksBySubjects.map(subject => ({
+            ...subject,
+            works: subject.works.filter(work => work.title.toLowerCase().includes(searchTerm.toLowerCase()))
+        }));
+
+        setSearchResults(filteredResults);
+    }, [searchTerm, booksBySubjects]);
+
+    const handleSearchChange = (event) => {
+        setSearchTerm(event.target.value);
+    };
+
     return (
         <>
-            {booksBySubjects.map((booksBySubject, index) => (
-                <div key={index}>
-                    <h3>{booksBySubject?.name}</h3>
-                    {booksBySubject?.works?.map((bookInfo, index) => (
-                        <div className="book-wrapper" key={index}>
-                            <div className="second">
-                            <h3>{bookInfo.title}</h3>
-                            <div className="image" key={index}>
-                                <img src={`http://covers.openlibrary.org/b/id/${bookInfo.cover_id}-M.jpg`} alt="cover_id" />
-                            </div>
-                            </div>
-                            {/* <h3>{bookInfo.cover_id}</h3> */}
+        <input type="search" name="" id="search" placeholder='Search books by title' value={searchTerm} onChange={handleSearchChange} />
+                {searchResults.map((booksBySubject, index) => (
+                    <div key={index}>
+                        <div>
+                            <h3 className='hmain'>{booksBySubject?.name}</h3>
                         </div>
-                    ))}
-                </div>
-            ))}
+                        <div className='ssmain'>
+                            {booksBySubject?.works?.map((bookInfo, index) => (
+                                <div className="book-wrapper" key={index}>
+                                        <h3 className='booktaetal'>{bookInfo.title}</h3>
+                                        <div className="image" key={index}>
+                                            <img src={`http://covers.openlibrary.org/b/id/${bookInfo.cover_id}-M.jpg`} alt="cover_id" />
+                                        </div>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                ))}
         </>
-    );
+    )
 }
 
 export default Main;
